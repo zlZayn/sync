f81bd932b6f96e049ded010c4f664e750cef0bdf
zlZayn <speaklzy@sina.com>
2026-05-08 23:30:35 +0800

    chore: clean up repo, keep only README

--- zhengdabei/code/script.r
+++
@@ -1,4 +0,0 @@
-source("func/spss_empty2na.R")
-
-a <- haven::read_sav("data/子女_预.sav") |>
-  spss_empty2na()
--- zhengdabei/func/spss_empty2na.r
+++
@@ -1,9 +0,0 @@
-spss_empty2na <- function(df) {
-  df |>
-    dplyr::mutate(
-      dplyr::across(
-        .cols = where(is.character),
-        .fns = ~ ifelse(is.na(.x) | .x == "", NA_character_, .x)
-      )
-    )
-}
